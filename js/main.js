6bb65c6d98caef8784e2a263d81173e18690eeed
Andreas <andreashev1872@gmail.com>
2025-11-30 20:03:48 +0700

    header and services

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+const swiper = new Swiper(".projects__slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
